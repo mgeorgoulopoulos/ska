67edd564682bc54b885ca2f6ff8c037b0e9ae5c0
SpaniardGR <ilias.k.cs@freemail.gr>
2017-04-15 01:47:25 +0300

    Board class init

---
+++ ska/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ska
+{
+    public class Board
+    {
+        public Piece[][] pieces;
+
+        public Board()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    pieces[i][j] = null;
+                }
+            }
+        }
+    }
+}
+
